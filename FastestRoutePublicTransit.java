89715db369099c2a129aaf9dbcd2777e9a0db65d
Adam vanWestrienen <avanwest@gmail.com>
2019-05-09 22:41:39 -0400

    add code for public transit

--- FastestRoutePublicTransit.java
+++ FastestRoutePublicTransit.java
@@ -1,7 +1,7 @@
 /**
  * Public Transit
- * Author: Your Name and Carolyn Yao
- * Does this compile? Y/N
+ * Author: Adam vanWestrienen and Carolyn Yao
+ * Does this compile? Y
  */
 
 /**
@@ -12,6 +12,7 @@
  */
 public class FastestRoutePublicTransit {
 
+  static boolean DEBUG = true;
   /**
    * The algorithm that could solve for shortest travel time from a station S
    * to a station T given various tables of information about each edge (u,v)
@@ -25,17 +26,146 @@ public class FastestRoutePublicTransit {
    * @return shortest travel time between S and T
    */
   public int myShortestTravelTime(
-    int S,
-    int T,
-    int startTime,
-    int[][] lengths,
-    int[][] first,
-    int[][] freq
+          int S,
+          int T,
+          int startTime,
+          int[][] lengths,
+          int[][] first,
+          int[][] freq
   ) {
-    // Your code along with comments here. Feel free to borrow code from any
-    // of the existing method. You can also make new helper methods.
-    return 0;
+
+    int numVertices = lengths[0].length;
+
+    // This is the array where we'll store all the final shortest times
+    int[] times = new int[numVertices];
+
+    // processed[i] will true if vertex i's shortest time is already finalized
+    Boolean[] processed = new Boolean[numVertices];
+
+    // Initialize all distances as INFINITE and processed[] as false
+    for (int v = 0; v < numVertices; v++) {
+      times[v] = Integer.MAX_VALUE;
+      processed[v] = false;
+    }
+
+    // Distance of source vertex from itself is always 0
+    times[S] = 0;
+
+    // Find shortest path to all the vertices
+    for (int count = 0; count < numVertices - 1 ; count++) {
+      // Pick the minimum distance vertex from the set of vertices not yet processed.
+      // u is always equal to source in first iteration.
+      // Mark u as processed.
+      int u = findNextToProcess(times, processed);
+      processed[u] = true;
+
+
+      // Update time value of all the adjacent vertices of the picked vertex.
+      for (int v = 0; v < numVertices; v++) {
+        int travelTime = 0;
+
+        // Do we have a valid link between u and v?
+        if (!processed[v] && lengths[u][v] != 0 && times[u] != Integer.MAX_VALUE) {
+          // Get the time we arrive at u.
+          int current = times[u] + startTime;
+
+          // Calculate the total time it will take for this route.  This takes into consideration
+          // the various timing constraints associated with the route.
+          if (DEBUG)
+            System.out.println("Calculate time from " + toAlpha(u) + " to " + toAlpha(v) + " where times[u] is "
+                    + times[u] + " and transit time is " + lengths[u][v] + " and frequency is " + freq[u][v] + " and start time is " + first[u][v]);
+          travelTime = calculateTime(current, lengths[u][v], freq[u][v], first[u][v]);
+
+          // Update only if we have found a shorter path to v.
+          if (times[u] + travelTime < times[v]) {
+            times[v] = times[u] + travelTime;
+            if (DEBUG) System.out.println("Time to get to " + toAlpha(v) + " is " + times[v]);
+          }
+        }
+      }
+    }
+    return times[T];
   }
+
+
+  /**
+   * Calculate the time it will take to travel from one station to the next.
+   * @param currentTime The time one arrives at the station.  An integer representing the number of minutes from 0530.
+   * @param travelTime An integer representing the time (minutes) spent on the train.
+   * @param frequency An integer representing the frequency (minutes) between trains.
+   * @param startTime The time (minutes) when the first train runs.
+   * @return The total time (minutes) it will take to travel the route.
+   */
+  private int calculateTime(int currentTime, int travelTime, int frequency, int startTime) {
+    int totalTime = 0;
+    if (DEBUG) System.out.println("Current Time: " + currentTime + " Travel time: " + travelTime + " Frequency: " + frequency + " Start time: " + startTime);
+    if ( travelTime != 0 && frequency != 0) {
+      // If we arrive at the station before the first train, then add in the time we must wait.
+      totalTime = currentTime < startTime ? startTime - currentTime : 0;
+
+      // If the trains are already running, then we only have to wait for the next
+      // scheduled train.
+      if (totalTime == 0 && currentTime != startTime) {
+        totalTime = (int) Math.abs((Math.ceil(currentTime / frequency) * frequency) - currentTime);
+        if (DEBUG) System.out.println("Time to wait for next train: " + totalTime);
+      }
+      else {
+        if (DEBUG) System.out.println("Time to wait for first train: " + totalTime);
+      }
+
+      // Add in the time it takes to travel the route.
+      totalTime += travelTime;
+    }
+    if (DEBUG) System.out.println("Calc total time: " + totalTime);
+    return totalTime;
+  }
+
+  /**
+   * Convert a station number into a letter.
+   * @param station The integer value of the station.
+   * @return The cooresponding letter as a string.
+   */
+  public static String toAlpha(int station) {
+    switch (station) {
+      case 0:
+        return "A";
+      case 1:
+        return "B";
+      case 2:
+        return "C";
+      case 3:
+        return "D";
+      case 4:
+        return "E";
+      case 5:
+        return "F";
+      case 6:
+        return "G";
+      case 7:
+        return "H";
+      case 8:
+        return "I";
+      case 9:
+        return "J";
+      case 10:
+        return "K";
+      case 11:
+        return "L";
+      case 12:
+        return "M";
+      case 13:
+        return "N";
+      case 14:
+        return "O";
+      case 15:
+        return "P";
+      case 16:
+        return "Q";
+      default:
+        return "?";
+    }
+  }
+
 
   /**
    * Finds the vertex with the minimum time from the source that has not been
@@ -60,7 +190,7 @@ public class FastestRoutePublicTransit {
   public void printShortestTimes(int times[]) {
     System.out.println("Vertex Distances (time) from Source");
     for (int i = 0; i < times.length; i++)
-        System.out.println(i + ": " + times[i] + " minutes");
+      System.out.println(i + ": " + times[i] + " minutes");
   }
 
   /**
@@ -111,19 +241,66 @@ public class FastestRoutePublicTransit {
   public static void main (String[] args) {
     /* length(e) */
     int lengthTimeGraph[][] = new int[][]{
-      {0, 4, 0, 0, 0, 0, 0, 8, 0},
-      {4, 0, 8, 0, 0, 0, 0, 11, 0},
-      {0, 8, 0, 7, 0, 4, 0, 0, 2},
-      {0, 0, 7, 0, 9, 14, 0, 0, 0},
-      {0, 0, 0, 9, 0, 10, 0, 0, 0},
-      {0, 0, 4, 14, 10, 0, 2, 0, 0},
-      {0, 0, 0, 0, 0, 2, 0, 1, 6},
-      {8, 11, 0, 0, 0, 0, 1, 0, 7},
-      {0, 0, 2, 0, 0, 0, 6, 7, 0}
+            { 0,  4,  0,  0,  0,  0,  0,  8,  0},
+            { 4,  0,  8,  0,  0,  0,  0, 11,  0},
+            { 0,  8,  0,  7,  0,  4,  0,  0,  2},
+            { 0,  0,  7,  0,  9, 14,  0,  0,  0},
+            { 0,  0,  0,  9,  0, 10,  0,  0,  0},
+            { 0,  0,  4, 14, 10,  0,  2,  0,  0},
+            { 0,  0,  0,  0,  0,  2,  0,  1,  6},
+            { 8, 11,  0,  0,  0,  0,  1,  0,  7},
+            { 0,  0,  2,  0,  0,  0,  6,  7,  0}
     };
     FastestRoutePublicTransit t = new FastestRoutePublicTransit();
     t.shortestTime(lengthTimeGraph, 0);
 
     // You can create a test case for your implemented method for extra credit below
+    int startTimeGraph[][] = new int[][]{
+            { 0,  1,  0,  0,  0,  0,  0,  1,  0},
+            { 1,  0,  1,  0,  0,  0,  0,  1,  0},
+            { 0,  1,  0,  1,  0,  1,  0,  0,  1},
+            { 0,  0,  1,  0,  1,  1,  0,  0,  0},
+            { 0,  0,  0,  1,  0,  1,  0,  0,  0},
+            { 0,  0,  1,  1,  1,  0,  1,  0,  0},
+            { 0,  0,  0,  0,  0,  1,  0,  1,  1},
+            { 1,  1,  0,  0,  0,  0,  1,  0,  1},
+            { 0,  0,  1,  0,  0,  0,  1,  1,  0}
+    };
+
+    int freqTimeGraph[][] = new int[][]{
+            { 0,  5,  0,  0,  0,  0,  0,  5,  0},
+            { 5,  0,  5,  0,  0,  0,  0,  5,  0},
+            { 0,  5,  0,  5,  0,  5,  0,  0,  5},
+            { 0,  0,  5,  0,  5,  5,  0,  0,  0},
+            { 0,  0,  0,  5,  0,  5,  0,  0,  0},
+            { 0,  0,  5,  5,  5,  0,  5,  0,  0},
+            { 0,  0,  0,  0,  0,  5,  0,  5,  5},
+            { 5,  5,  0,  0,  0,  0,  5,  0,  5},
+            { 0,  0,  5,  0,  0,  0,  5,  5,  0}
+    };
+
+    System.out.println("\nRun tests using time considerations with Dijkstra's algorithm.");
+    runTest(t,0, 3, 5, lengthTimeGraph, startTimeGraph, freqTimeGraph);
+    runTest(t, 1, 4, 0, lengthTimeGraph, startTimeGraph, freqTimeGraph);
+
+
+  }
+
+  /**
+   * Run the myShortestTravelTime method.
+   * @param t An instance of the FastestRoutePublicTransit class.
+   * @param startStation The station to start at.
+   * @param endStation The station to terminate at.
+   * @param startTime The time to start the journey (0 = 0530)
+   * @param lengthTimeGraph An array of the times the trains take per route.
+   * @param startTimeGraph An array of when the trains start service for a route.
+   * @param freqTimeGraph An array of the frequency of train service on the route.
+   */
+  public static void runTest(FastestRoutePublicTransit t, int startStation, int endStation, int startTime, int[][] lengthTimeGraph, int[][] startTimeGraph, int[][] freqTimeGraph ) {
+
+    System.out.println("\nCalculate time from station " + toAlpha(startStation) + " to station " + toAlpha(endStation));
+    int totalTime = t.myShortestTravelTime(startStation, endStation, startTime, lengthTimeGraph, startTimeGraph, freqTimeGraph);
+    System.out.println("Start: " + toAlpha(startStation) + " End: " + toAlpha(endStation) + " Start time: " + startTime + " Total time: " + totalTime);
+
   }
 }
